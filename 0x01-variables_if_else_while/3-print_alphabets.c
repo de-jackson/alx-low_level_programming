c1bb9880f9cdbbc9ef015effc0b002f2c46bbc2b
root <root@DESKTOP-T2SK2H8.localdomain>
2021-08-27 16:27:09 +0300

    Solving task 3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Print lower alphabet
+ *
+ * Return : 0 on success
+ */
+
+int main(void)
+{
+	char c = 'a';
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
