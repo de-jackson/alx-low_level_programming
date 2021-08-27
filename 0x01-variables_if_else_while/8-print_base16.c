453d9140b385c6c615e80ef56cd957c66d697df6
root <root@DESKTOP-T2SK2H8.localdomain>
2021-08-27 17:52:22 +0300

    Solving task 8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers of base 16.
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = '0'; letter <= '9'; letter++)
+	{
+		putchar(letter);
+
+	}
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
