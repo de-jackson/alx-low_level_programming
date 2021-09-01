a7de8f54c7c919c4d23196cc5b1876a0b8898b9f
root <root@DESKTOP-T2SK2H8.localdomain>
2021-09-01 11:49:26 +0300

    Solving task 0. _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,22 +1,21 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
- * main - prints Holberton, Followed by a newline
- * Return: Always 0 on (success)
+ * main - Check description
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0.
  */
 
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	char word[9] = "Holberton";
+	int i;
 
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
