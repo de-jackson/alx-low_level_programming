2f8c1592414f2f7731524bfb88a2d6e1ac85280a
root <root@DESKTOP-T2SK2H8.localdomain>
2021-09-02 12:42:44 +0300

    Solving infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,6 +2,7 @@
 
 /**
  * main - causes an infinite loop
+ *
  * Return: 0
  */
 
@@ -13,12 +14,11 @@ int main(void)
 
 	i = 0;
 
-	/*while (i < 10)*/
-	/*{*/
+	/* while (i < 10)*/
+	/* {*/
 	/*	putchar(i);*/
-	/*}*/
-	
+	/* }*/
+
 	printf("Infinite loop avoided! \\o/\n");
-	
 	return (0);
 }
