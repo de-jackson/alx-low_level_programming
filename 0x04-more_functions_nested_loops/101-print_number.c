ce8272372139eab62cb82edca630b74841968ef5
root <root@DESKTOP-T2SK2H8.localdomain>
2021-09-03 15:36:48 +0300

    Solving task 12. a function that prints an integer.

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number
+ * @n: Input number
+ * Return: Nothing
+ */
+
+void print_number(int n)
+{
+	int len, res, i, temp, expo;
+
+	res = n;
+	expo = len =  1;
+	if (res < 0)
+	{
+		res *= -1;
+		_putchar('-');
+	}
+
+	temp = res;
+	while (temp >= 10)
+	{
+		len++;
+		temp /= 10;
+	}
+
+	for (i = 1; i < len; i++)
+	{
+		expo *= 10;
+	}
+
+	while (expo > 1)
+	{
+		_putchar((res / expo) % 10 + '0');
+		expo /= 10;
+	}
+	_putchar(res % 10 + '0');
+}
