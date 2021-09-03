699db4fdfe80f8c6066589b3f64c0e0b7ce9ed51
root <root@DESKTOP-T2SK2H8.localdomain>
2021-09-03 13:30:21 +0300

    Solving task 6. function that draws a straight line in the terminal.

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line (n) characters long.
+ * @n: the number of underscores to print.
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+
+	}
+	_putchar('\n');
+}
