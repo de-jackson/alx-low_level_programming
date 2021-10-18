06900d11da56f0e0b071b640ce40c94bfd9c6b48
de-jackson <jackyeyamu@gmail.com>
2021-10-18 17:37:23 +0300

    Solving task 0. Tread lightly, she is near

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* __MAIN_H__ */
