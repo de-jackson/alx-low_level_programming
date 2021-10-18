352b0970b8b740badbb2b95d663de6825242a401
de-jackson <jackyeyamu@gmail.com>
2021-10-18 18:08:25 +0300

    Solving task 1. Under the snow

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/uio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
 
 #endif /* __MAIN_H__ */
