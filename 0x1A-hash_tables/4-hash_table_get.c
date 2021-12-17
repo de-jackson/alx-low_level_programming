abac99b020b9a204037eb0449ca3d0b728775d03
de-jackson <jackyeyamu@gmail.com>
2021-12-17 21:48:40 +0300

    Solving task 4. >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -24,11 +24,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	}
 
 	index = key_index((const unsigned char *)key, ht->size);
-	if ((ht->array)[index] == NULL)
+	tmp = ht->array[index];
+	if (tmp == NULL)
 	{
 		return (NULL);
 	}
-	tmp = ht->array[index];
 	while (!value)
 	{
 		if (strcmp(tmp->key, key) == 0)
